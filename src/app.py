072be55ce670e8f9c65f4c0cddfe3496d1dab29f
Sonjeongbeom <sjb990221@gmail.com>
2023-08-30 12:29:36 +0900

    Add: exception handling

--- src/app.py
+++ src/app.py
@@ -7,46 +7,50 @@ app = Flask(__name__)
 CORS(app, resources={r"/": {"origins" : "*"}})
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def health_check():
+    return 'OK!'
 
 @app.route('/dream/generate', methods = ['POST'])
 def generate_dream() :
-    body = request.get_json()
-    utterance = body['utterance']
-    modelResult = generate(utterance)
+    try : 
+        body = request.get_json()
+        utterance = body['utterance']
+        modelResult = generate(utterance)
 
-    if modelResult.error == True :
+        if modelResult.error == True :
+            return Response(modelResult.message, status = HTTPStatus.BAD_REQUEST)
+    
         return jsonify({
-            "status" : HTTPStatus.INTERNAL_SERVER_ERROR,
+            "message" : modelResult.message,
+            "dream_title" : modelResult.data['dream_title'],
+            "eng_dream_title" : modelResult.data['eng_dream_title'],
+            "possible_meanings" : modelResult.data['possible_meanings'],
+            "recommended_tarot_card" : modelResult.data['recommended_tarot_card'],
+            "image_url" : modelResult.data['image_url']
         })
     
-    return jsonify({
-        "status" : HTTPStatus.OK,
-        "dream_title" : modelResult.data['dream_title'],
-        "eng_dream_title" : modelResult.data['eng_dream_title'],
-        "possible_meanings" : modelResult.data['possible_meanings'],
-        "recommended_tarot_card" : modelResult.data['recommended_tarot_card'],
-        "image_url" : modelResult.data['image_url']
-    })
+    except Exception as e :
+        return Response(str(e), status = HTTPStatus.INTERNAL_SERVER_ERROR)
+
 
 @app.route('/dream/regenerate', methods = ['POST'])
 def regenerate_dream() :
-    body = request.get_json()
-    dream = body['dream']
-    tarot_card = body['tarot_card']
-    modelResult = regenerate(dream, tarot_card)
+    try :     
+        body = request.get_json()
+        dream = body['dream']
+        tarot_card = body['tarot_card']
+        modelResult = regenerate(dream, tarot_card)
 
-    if modelResult.error == True :
+        if modelResult.error == True :
+            return Response(modelResult.message, status = HTTPStatus.INTERNAL_SERVER_ERROR)
+        
         return jsonify({
-            "status" : HTTPStatus.INTERNAL_SERVER_ERROR,
+            "messsage" : modelResult.message,
+            "image_url" : modelResult.data['image_url']
         })
-    
-    return jsonify({
-        "status" : HTTPStatus.OK,
-        "image_url" : modelResult.data['image_url']
-    })
 
+    except Exception as e :
+        return Response(str(e), status = HTTPStatus.INTERNAL_SERVER_ERROR)
 
 if __name__ == "__main__":
     app.run(debug = True, port = 5000, host = '0.0.0.0')
--- src/model_pipeline.py
+++ src/model_pipeline.py
@@ -1,11 +1,8 @@
 from PIL import Image
 import openai
 import io
-import sys
-import requests
 import json
 import os
-import time
 from dotenv import load_dotenv
 from papago import kor_to_eng_translation, eng_to_kor_translation
 
@@ -23,116 +20,126 @@ gpt_template = {
 }
 
 def executeGpt(text) : 
-    response = openai.ChatCompletion.create(
+    try :
+        response = openai.ChatCompletion.create(
         model = "gpt-3.5-turbo",
         messages = [
-        {"role": "system", "content": "You are a helpful assistant who interprets dreams and recommends appropriate one tarot card among 22  tarot cards."},
-        {"role": "user", "content": """
-         I had a dream that I had a car accident on the highway.
-         Please interpret this dream and recommend 1 out of 22 tarot cards. (Judgement, Justice, Strength, Temperance, The Chariot, The Death, The Devil, The Emperor, The Empress, The Fool, The Hanged Man, The Hermit, The Hierophant, The High Priestess, The Lovers, The Magician, The Moon, The Star, The Sun, The Tower, The World, Wheel of Fortune)
-         Give me the response in JSON format and the format must follow below.""" },
-        {"role": "assistant", "content": json.dumps(gpt_template)},
-        {"role": "user", "content": text + " Please interpret this dream and recommend 1 out of 22 tarot cards. Give me the resposne in JSON format."}   
-    ])
+            {"role": "system", "content": "You are a helpful assistant who interprets dreams and recommends appropriate one tarot card among 22  tarot cards."},
+            {"role": "user", "content": """
+            I had a dream that I had a car accident on the highway.
+            Please interpret this dream and recommend 1 out of 22 tarot cards. (Judgement, Justice, Strength, Temperance, The Chariot, The Death, The Devil, The Emperor, The Empress, The Fool, The Hanged Man, The Hermit, The Hierophant, The High Priestess, The Lovers, The Magician, The Moon, The Star, The Sun, The Tower, The World, Wheel of Fortune)
+            Give me the response in JSON format and the format must follow below.""" },
+            {"role": "assistant", "content": json.dumps(gpt_template)},
+            {"role": "user", "content": text + " Please interpret this dream and recommend 1 out of 22 tarot cards. Give me the resposne in JSON format."}   
+        ])
 
-    if response :
-        return response['choices'][0]['message']['content']
-    else :
-        return None
+        if response :
+            return response['choices'][0]['message']['content']
+        else :
+            return None
+    
+    except Exception as e :
+        raise e
+    
 
 def executeDalle(dream, tarot_card) :
-    tarot_card = tarot_card.lower().replace(' ', '_')
-    image = Image.open("./img/image/{tarot_card}.png".format(tarot_card = tarot_card)).convert("RGBA")
-    mask = Image.open("./img/mask/{tarot_card}_mask.png".format(tarot_card = tarot_card)).convert("RGBA")
-    b1 = io.BytesIO()
-    b2 = io.BytesIO()
+    try : 
+        print('dream', dream, 'tart_card', tarot_card)
+        tarot_card = tarot_card.lower().replace(' ', '_')
+        image = Image.open("./img/image/{tarot_card}.png".format(tarot_card = tarot_card)).convert("RGBA")
+        mask = Image.open("./img/mask/{tarot_card}_mask.png".format(tarot_card = tarot_card)).convert("RGBA")
+        b1 = io.BytesIO()
+        b2 = io.BytesIO()
 
-    image.save(b1, format = "PNG")
-    b1.seek(0)
-    mask.save(b2, format = "PNG")
-    b2.seek(0)
+        image.save(b1, format = "PNG")
+        b1.seek(0)
+        mask.save(b2, format = "PNG")
+        b2.seek(0)
 
-    response = openai.Image.create_edit(
-        image = b1,
-        mask = b2,
-        prompt = "Draw about {dream} by medieval drawing style.".format(dream = dream),
-        n = 1,
-        size = "1024x1024",
-    )
+        response = openai.Image.create_edit(
+            image = b1,
+            mask = b2,
+            prompt = "Draw about {dream} by medieval drawing style.".format(dream = dream),
+            n = 1,
+            size = "1024x1024",
+        )
 
-    # if response.status_code != 200 :
-    #     # 에러 처리
-    #     sys.exit(1)
-    if response :
-        return response['data'][0]['url']
-    else :
-        return None
+        if response :
+            return response['data'][0]['url']
+        else :
+            return None
+        
+    except Exception as e :
+        raise e
 
 def regenerate(dream, tarot_card) :
-    global OPEN_AI_API_KEY, OPEN_AI_API_URL
+    try :
+        global OPEN_AI_API_KEY, OPEN_AI_API_URL
 
-    load_dotenv('../.env')
-    OPEN_AI_API_KEY = os.getenv("OPEN_AI_API_KEY")
-    OPEN_AI_API_URL = os.getenv("OPEN_AI_API_URL")
+        load_dotenv('../.env')
+        OPEN_AI_API_KEY = os.getenv("OPEN_AI_API_KEY")
+        OPEN_AI_API_URL = os.getenv("OPEN_AI_API_URL")
 
-    openai.api_key = OPEN_AI_API_KEY
+        openai.api_key = OPEN_AI_API_KEY
 
-    image_url = executeDalle(dream, tarot_card)
-    data = {
-        'image_url' : image_url
-    }
-    return ModelResult(False, data)
+        image_url = executeDalle(dream, tarot_card)
+        if image_url == None : 
+            return ModelResult(True, "No Dalle Result")
+        
+        data = {
+            'image_url' : image_url
+        }
+        return ModelResult(False, "success", data)
+    
+    except Exception as e :
+        raise e;
 
 
 def generate(utterance) :
-    global OPEN_AI_API_KEY, OPEN_AI_API_URL
+    try :
+        global OPEN_AI_API_KEY, OPEN_AI_API_URL
 
-    load_dotenv('../.env')
-    OPEN_AI_API_KEY = os.getenv("OPEN_AI_API_KEY")
-    OPEN_AI_API_URL = os.getenv("OPEN_AI_API_URL")
+        load_dotenv('../.env')
+        OPEN_AI_API_KEY = os.getenv("OPEN_AI_API_KEY")
+        OPEN_AI_API_URL = os.getenv("OPEN_AI_API_URL")
 
-    openai.api_key = OPEN_AI_API_KEY
+        openai.api_key = OPEN_AI_API_KEY
 
-    eng_utterance = kor_to_eng_translation(utterance)
-    eng_gpt_result = json.loads(executeGpt(eng_utterance))
+        eng_utterance = kor_to_eng_translation(utterance)
+        eng_gpt_result = json.loads(executeGpt(eng_utterance))
 
-    eng_gpt_result = json.loads(executeGpt(utterance))
-    if eng_gpt_result == None :
-        return ModelResult(True, None)
-    
-    if 'recommended_tarot_card' in eng_gpt_result :
-        recommended_tarot_card = eng_gpt_result['recommended_tarot_card']
-    else :
-        return ModelResult(True, None)
+        if eng_gpt_result == None :
+            return ModelResult(True, "No GPT Result")
+        
+        if 'recommended_tarot_card' not in eng_gpt_result or 'dream_title' not in eng_gpt_result or 'possible_meanings' not in eng_gpt_result :
+            return ModelResult(True, "No GPT Result")
 
-    if 'dream_title' in eng_gpt_result : 
         dream_title = eng_gpt_result['dream_title']
-    else :
-        dream_title = ""
-
-    if 'possible_meanings' in eng_gpt_result :
         possible_meanings = eng_gpt_result['possible_meanings']
-    else :
-        possible_meanings = []
-    
-    image_url = executeDalle(dream_title, recommended_tarot_card)
-    # meaining_keys = [meaning.split(':')[0] for meaning in possible_meanings]
-    kor_dream_title = eng_to_kor_translation(dream_title)
-    kor_possible_meanings = [eng_to_kor_translation(meaning) for meaning in possible_meanings]
-    kor_possible_meanings = [meaning for meaning in kor_possible_meanings if len(meaning) > 0]
+        meaning_key = possible_meanings[0].split(':')[0]
+        recommended_tarot_card = eng_gpt_result['recommended_tarot_card']
+        # image_url = executeDalle(dream_title, recommended_tarot_card)
+        image_url = executeDalle(dream_title + ', ' + meaning_key, recommended_tarot_card)
+        kor_dream_title = eng_to_kor_translation(dream_title)
+        kor_possible_meanings = [eng_to_kor_translation(meaning) for meaning in possible_meanings]
+        kor_possible_meanings = [meaning for meaning in kor_possible_meanings if len(meaning) > 0]
 
-    data = {
-        'dream_title' : kor_dream_title,
-        'eng_dream_title' : dream_title,
-        'possible_meanings' : kor_possible_meanings,
-        'recommended_tarot_card' : recommended_tarot_card,
-        'image_url' : image_url
-    }
-    return ModelResult(False, data)
+        data = {
+            'dream_title' : kor_dream_title,
+            'eng_dream_title' : dream_title,
+            'possible_meanings' : kor_possible_meanings,
+            'recommended_tarot_card' : recommended_tarot_card,
+            'image_url' : image_url
+        }
+        return ModelResult(False, "success", data)
+
+    except Exception as e :
+        raise e
 
 class ModelResult :
-    def __init__(self, error, data = None) :
+    def __init__(self, error, message = None, data = None) :
         self.error = error
+        self.message = message
         self.data = data
 
     def __str__(self) :
